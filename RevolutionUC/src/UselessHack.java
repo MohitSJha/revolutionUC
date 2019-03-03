5a496cab79f3e8e0d004a70ac842c8f47055a5bc
mohit <mohit@MohitJha.wifi.uc.edu>
2019-03-03 03:04:54 -0500

    Started the RevolutionUC project

---
+++ RevolutionUC/src/UselessHack.java
@@ -0,0 +1,41 @@
+import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+
+public class UselessHack extends Application implements EventHandler
+{
+	
+	Scene mainPage, firstPage, secondPage, thirdPage;
+
+	public static void main(String[] args)
+	{
+		// Launches the program as a JavaFX application
+		launch(args);
+
+	}
+
+	@Override
+	public void handle(Event arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
